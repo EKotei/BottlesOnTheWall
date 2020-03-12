8079289fabb08de3ea8b54e397ce5964a377f4dc
Edmund Kotei <edmundkotei@gmail.com>
2020-03-12 10:44:20 -0400

    The mind tires before the body

---
+++ src/main/java/myfirstpackage/Main.java
@@ -0,0 +1,56 @@
+package myfirstpackage;
+
+public class Main {
+
+//    public static void printUserName(String userName){
+//        System.out.println("Welcome to Wakanda " + userName);
+//    }
+
+//    public static void main(String[] args) {
+//        //we invoked or called or used our method
+//        printUserName("Crackhead Billy");
+//        printUserName("Hope you aint got corona virus");
+
+
+//    public static void addNum(int num) {
+//        System.out.println(4 * num);
+//    }
+//
+//    public static void main(String[] args) {
+////        addNum(2);
+////        multNum(5, 7);
+//        calculateSalePrice(100, 0.15f);
+//        //aaaa(10, 5.0f);
+//    }
+//
+//
+//
+//    public static void multNum(int number, int number2) {
+//        System.out.println(number * number2);
+//    }
+//
+//    public static void calculateSalePrice(int original, float discount) {
+//        float discountSaleThing = (original * discount);
+//        System.out.println(original - discountSaleThing);
+//    }
+//
+//    public static void aaaa(int num, float num2) {
+//        System.out.println(num + num2);
+//        float sum = num + num2;
+//        System.out.println(sum + num);
+//    }
+//public static void main(String[] args) {
+
+    //    int x = 20;
+//    System.out.println(x);
+//    int y = 35;
+//    System.out.println(y);
+// float x = 5.0f;
+// int y = 6;
+// char myLetter = 5;
+// short s = 32;
+    public static void main(String[] args) {
+
+    }
+
+}
